d3a0fa143d35a5555b4cf38ae23092525d744f41
XLR <odikachinedu@gmail.com>
2020-09-04 12:52:03 +0100

    scaffolding login + cleanup template

---
+++ client/src/views/registration.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mt-12 bg-white flex flex-col items-center">
+        <img class="logo w-24" alt="logo" src="@/assets/vend.svg" />
+        <span class="bg-green mt-4"> Services services</span>
+
+        <div class="mt-6 sm:text-xs">
+            <input name="email" type="text" placeholder="Enter Email" class="input">
+            <input name="username" type="text" placeholder="Username" class="input">
+            <input name="password" type="password" placeholder="Password" class="input">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "registration"
+
+};
+</script>
