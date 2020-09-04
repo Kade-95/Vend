4bbcd6d22a55eed4ba42e1635e7962311184ae92
XLR <odikachinedu@gmail.com>
2020-09-04 12:50:47 +0100

    Fix app routing.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    
+
     <router-view />
   </div>
-</template>
+</template> 
 
 <style>
 #app {
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,27 @@
+
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="nav" class="flex flex-row justify-between">
+    <img class="logo ml- w-16" alt="Vue logo" src="@/assets/vend.svg" />
+    <img class="logo ml- w-6" alt="nav" src="@/assets/menu.svg" />
+
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+    </div>
+    <registration />
   </div>
 </template>
 
 <script>
+import registration from "../views/registration.vue"
+
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    registration
   }
 };
 </script>
