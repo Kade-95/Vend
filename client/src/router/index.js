9c36bb21fed5e08bbb9a3c4d0c41bbe903d680e8
XLR <odikachinedu@gmail.com>
2020-10-16 05:38:55 +0100

    New routing rules.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,39 +1,60 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import LandingPage from "../views/LandingPage.vue";
+import LandingPage from "../views/LandingPage.vue";
+import Find from "../components/Finder.vue";
+import Home from "../views/Home.vue"
+import Blog from "../components/Blog.vue";
+import Negotiations from "../components/Negotiations.vue";
+
+
 
 Vue.use(VueRouter);
 
+var sessionActive = true;
+var validUser;
+
 const routes = [
   {
-    path: "/dashboard",
-    name: "LandingPage",
-    component: function() {
-      var notloggedIn = true;
-      if (notloggedIn) {
-        return import(/* webpackChunkName: "" */ "../views/LandingPage.vue");
-      }
-    }
-  },
-  {
     path: "/",
+    name: "Start",
+    component: function() {
+
+      if (!sessionActive) {
+
+        console.log("land");
+
+        return LandingPage
+      }
+      console.log("home");
+      return Home
+
+    },
+
+    children: [
+      {
+        path: "/find",
+        component: Find
+      },
+      {
+        path: "/blog",
+        component: Blog
+      },
+      {
+        path: "/negotiations",
+        component: Negotiations
+      }
+
+    ],
+
+    beforeEnter: (to, from, next) => {
+      // ...
+      if (!sessionActive && validUser) next({ name: 'Registration' })
+      else next()
+    }
+  },
+  {
+    path: "/register",
     name: "Registration",
-
-    component: function() {
-      return import(/* webpackChunkName: "" */ "../views/Registration.vue");
-    }
-  },
-  {
-    path: "/home",
-    name: "home",
-
-    component: function() {
-      return import(/* webpackChunkName: "" */ "../views/Home.vue");
-    }
-  },
-  {
-    path: "/test",
-    name: "test",
 
     component: function() {
       return import(/* webpackChunkName: "" */ "../views/Registration.vue");
@@ -44,5 +65,6 @@ const routes = [
 const router = new VueRouter({
   routes
 });
+
 
 export default router;
